2b22717ba0da08b4cdf70e60352acd96dae00ca1
Trkesa <22311a12g6@it.sreenidhi.edu.in>
2026-02-20 07:29:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (82.01%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -1,10 +1,9 @@
 class Solution {
     public int hammingWeight(int n) {
-        int count =0;
-        for(int i=0;i<32;i++){
-            if((n & (1<<i))!=0){
-                count++;
-            }
+        int count=0;
+        while(n>0){
+            n=n & n-1;
+            count++;
         }
         return count;
     }
